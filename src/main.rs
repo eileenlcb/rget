f03518266fbed18ef43b27872832b99ecbe11924
Liu Can <liucan@shopee.com>
2023-09-04 17:31:01 +0800

    code modify

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+#![allow(unused)]
+mod utils;
+
 use clap::{App, Arg};
+use reqwest::blocking::Client;
+use failure::{format_err, Fallible};
 
 fn main(){
     let matches = App::new("Rget")
@@ -17,3 +22,24 @@ fn main(){
 }
 
 
+fn download(target:&str,quiet_mode:bool)->Result<(),Box<dyn::std::error::Error>>{
+    let url = utils::parse_url(target)?;
+    let client = Client::new();
+    let mut resp = client.get(url.as_ref()).send()?;
+    
+    match url.scheme() {
+        "ftp" => ftp_download(),
+        "http" | "https" => http_download(),
+        _ => utils::gen_error(format!("unsupported url scheme '{}'", url.scheme())),
+    };
+
+    Ok(())
+}
+
+fn ftp_download() -> Fallible<()> {
+    Ok(())
+}
+
+fn http_download() -> Fallible<()>{
+    Ok(())
+}
---
+++ src/utils.rs
@@ -0,0 +1,23 @@
+#![allow(unused)]
+use failure::{bail, Fallible};
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::io;
+use std::path::Path;
+use url::{ParseError, Url};
+
+
+pub fn parse_url(url: &str) -> Result<Url,ParseError> {
+    match Url::parse(url){
+        Ok(url) => Ok(url),
+        Err(error) if error == ParseError::RelativeUrlWithoutBase => {
+            let url_with_base = format!("{}{}", "http://", url);
+            Url::parse(&url_with_base)
+        }
+        Err(error) => Err(error),
+    }
+}
+
+pub fn gen_error(msg:String) -> Fallible<()>{
+    bail!(msg)
+}
